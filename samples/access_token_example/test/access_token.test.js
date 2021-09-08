1f6656c922732056bb506de40e554dc599f589cc
codepriest <codepriest.gudana@gmail.com>
2021-09-08 10:31:27 +0100

    Fix resource path of image for gcs upload.

--- samples/access_token_example/test/access_token.test.js
+++ samples/access_token_example/test/access_token.test.js
@@ -153,7 +153,7 @@ it('Generate gcp access token, use gcp access token to create gcs bucket upload 
 
     // Upload Data to GCS bucket
     const dataName = 'testFILE';
-    const binaryData = readFileSync('../resources/logo.png');
+    const binaryData = readFileSync('.././resources/logo.png');
     const uploadGcsRequestUrl = `https://storage.googleapis.com/upload/storage/v1/b/${bucketName}/o?uploadType=media&name=${dataName}`;
     const uploadGcsOptions = {
       url: uploadGcsRequestUrl,
